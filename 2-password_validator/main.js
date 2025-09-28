0d796376d69494fc99651ea7f6c049add8aea01b
Ali Örer <129732909+AliOrer@users.noreply.github.com>
2025-09-29 00:37:58 +0200

    edited-and-removed-some-useless-codes

--- 2-password_validator/main.js
+++ 2-password_validator/main.js
@@ -1,5 +1,5 @@
-import React , { useState } from 'react';
-import validator from 'validator';
+import React , { useState } from "react";
+import validator from "validator";
 
 
 const App = () => {
@@ -8,22 +8,27 @@ const App = () => {
     const validate = (value) =>
     {
       if (validator.isStrongPassword(value, {
-        minLength: 8, minLowercase:1, minUppercase:1, minSymbols: 1
-      })) { setErrorMessage('Is Strong Password')
-        }
-        else{
+        minLength: 8,
+        minLowercase:1,
+        minUppercase:1,
+        minNumbers:1,
+        minSymbols: 1
+      })
+      )   { setErrorMessage('Is Strong Password')
+        } else{
           setErrorMessage('Is Not Strong Password')
         }
-    }
+    };
     
-    return(
-      <div style={{marginLeft'200px'}}>
+    return (
+      <div style={{marginLeft :'200px',}}>
         <pre>
           <h2>Checking Password Strength in ReactJS</h2>
           <span>Enter Password: </span>
-          <input type="text" onChange={(e) => validate(e.target.value)}></input>
-          <br>
-            {errorMessage === '' ? null: <span style= {{fontWeight: 'bold', color='red'}}>
+          <input type="text" onChange = {(e) => validate(e.target.value)}> </input>
+          <br/>
+            {errorMessage === '' ? null: <span style= {{fontWeight: 'bold', color:'red',}}>
+            {errorMessage}</span>}
         </pre>
       </div>
       );
